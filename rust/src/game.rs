e8e405d53cd7ee93fdaf8930abd6e41314e624cf
yan <3200828584@qq.com>
2022-03-19 09:59:40 +0800

    complete ecs template

--- rust/src/game.rs
+++ rust/src/game.rs
@@ -1,25 +1,34 @@
+use bevy::prelude::{DespawnRecursiveExt, Entity};
 use gdnative::prelude::*;
 use gdrust::ecs::app::with_world;
+use gdrust::ecs::engine_sync::components::{GameNode, GodotObjRef};
 use gdrust::ecs::engine_sync::events::spawn_game;
-use gdrust::ecs::engine_sync::resources::GameOver;
 use gdrust::macros::gdrust;
 
 #[gdrust(extends = Spatial)]
-pub struct Game;
+pub struct Game {
+    pub entity: Option<Entity>,
+}
+
 #[methods]
 impl Game {
     #[export]
     fn _ready(&mut self, owner: TRef<Spatial>) {
         with_world(|w| {
-            spawn_game(w, owner.upcast::<Node>().claim());
+            let entity = w
+                .spawn()
+                .insert(GodotObjRef::new(owner.upcast::<Node>().claim()))
+                .insert(GameNode)
+                .id();
+            spawn_game(w, entity);
+            self.entity = Some(entity);
         });
     }
 
     #[export]
     fn free(&mut self, owner: TRef<Spatial>) {
         with_world(|w| {
-            let mut game_over = w.get_resource_mut::<Option<GameOver>>().unwrap();
-            *game_over = Some(GameOver::Win);
+            w.entity_mut(self.entity.unwrap()).despawn_recursive();
         });
     }
 }
